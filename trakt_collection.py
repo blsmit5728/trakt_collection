68d28292cd1054850e84e69492d8bb9a3ee09703
Brandon Smith <blsmit5728@gmail.com>
2014-02-08 10:33:50 -0500

    Now you can add your own api_key, also, this will now work with 99% of all tv shows. Only thing not currently supportted is seasons as years...

--- trakt_collection.py
+++ trakt_collection.py
@@ -6,67 +6,62 @@ import sys
 import getopt
 import os
 
-#trakt.tv.setup(apikey='a115a168132e06e8b05ab9a58790390d',username='blsmit5728',password='baseball1')
-#t = trakt.tv.show.season('the-walking-dead',3)
-#x = json.dumps(t,ensure_ascii=False)
-#y = json.loads(x)
-#print y
-#for c in y:
-#    print c['episode']
-#    print c['in_collection']
-#    print c['tvdb_id']
+def sanitize_for_trakt(series_name):
+    if '(' in series_name:
+        series_name = series_name.replace("(","")
+        series_name = series_name.replace(")","")
+    if '.' in series_name:
+        series_name = series_name.replace(".","")
+    if '\'' in series_name:
+        series_name = series_name.replace("'","")
+    if '!' in series_name:
+        series_name = series_name.replace("!","")
+    if '&' in series_name:
+        series_name = series_name.replace("&","and")
+    if '-' in series_name:
+        series_name = series_name.replace("-","")
+    return series_name
 
 def main(argv):
     trakt_user = ""
     trakt_pass = ""
     search_dir = ""
+    trakt_api_key = ""
     grammar = "kant.xml"                 
     try:                                
-        opts, args = getopt.getopt(argv, "d:u:p:", ["dir=", "username=", "password="]) 
+        opts, args = getopt.getopt(argv, "a:d:u:p:", ["api_key=","dir=", "username=", "password="]) 
     except getopt.GetoptError:           
         sys.exit(2)
     for opt, arg in opts:
         if opt in ("-d","--dir"):
-            print "Directory=" + arg
             search_dir = arg
         elif opt in ("-u","--username"):
             trakt_user = arg
-            print trakt_user
         elif opt in ("-p","--password"):
             trakt_pass = arg
-            print trakt_pass
+        elif opt in ("-a","--api","--api_key"):
+            trakt_api_key = arg
 
     names = os.listdir(search_dir)
     for n in names:
-        trakt.tv.setup(apikey='a115a168132e06e8b05ab9a58790390d',username=trakt_user,password=trakt_pass)
-        #t = trakt.tv.show.season('the-walking-dead',3)
+        trakt.tv.setup(apikey=trakt_api_key,username=trakt_user,password=trakt_pass)
         print "Show: " + n
-        if '(' in n:
-            n = n.replace("(","")
-            n = n.replace(")","")
-        elif '.' in n:
-            n = n.replace(".","")
-        elif '\'' in n:
-            n = n.replace("'","")
-        elif '!' in n:
-            n = n.replace("!","")
-        elif '&' in n:
-            n = n.replace("&","")
+        n = sanitize_for_trakt(n) 
         seas = trakt.tv.show.seasons(n.replace(" ", "-"))
         seas_j = json.dumps(seas,ensure_ascii=False)
         seas_jload = json.loads(seas_j)
         seasons = int(seas_jload[0]['season'])
-        for i in range(1,seasons+1):
-            print i
-            t = trakt.tv.show.season(n.replace(" ", "-"),str(i))
-            x = json.dumps(t,ensure_ascii=False)
-            y = json.loads(x)
-        #print y
-            for c in y:
-        #pprint(c)
-        #print c['episode']
-            #print c['in_collection']
-                print c['tvdb_id']
+        if int(seasons) > 2000:
+            print "This show probably starts with a yearly season...fuck"
+        else:   
+            for i in range(1,seasons+1):
+                t = trakt.tv.show.season(n.replace(" ", "-"),str(i))
+                x = json.dumps(t,ensure_ascii=False)
+                y = json.loads(x)
+                for c in y:
+                    if "Brooklyn" in n:
+                        print c
+                    print "Is " + str(n) + " [S" + str(c['season']) + "E" + str(c['episode']) +  "] in your collection? " + str(c['in_collection'])
 
 if __name__ == "__main__":
     main(sys.argv[1:])
